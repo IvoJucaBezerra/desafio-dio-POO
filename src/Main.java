76ab84ee8f822f2307ef5058afa4ab9fd7cd0c7b
Ivo Juca <ivojucabezerra@hotmail.com>
2023-04-28 11:00:00 -0300

    Finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -23,5 +25,46 @@ public class Main {
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devIvo = new Dev();
+        devIvo.setNome("Ivo Jucá");
+        devIvo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Ivo Jucá:" + devIvo.getConteudosInscritos());
+        devIvo.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Ivo Jucá:" + devIvo.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Ivo Jucá:" + devIvo.getconteudosConcluidos());
+        System.out.println("XP:" + devIvo.calcularTotalXp());
+
+        System.out.println("------------");
+
+        Dev devMaria = new Dev();
+        devMaria.setNome("Maria Mari");
+        devMaria.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Maria Mari:" + devMaria.getConteudosInscritos());
+        devMaria.progredir();
+        devMaria.progredir();
+        devMaria.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Maria Mari:" + devMaria.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Maria Mari:" + devMaria.getconteudosConcluidos());
+        System.out.println("XP:" + devMaria.calcularTotalXp());
+
+
+
+
+
+
+
+
+
+
     }
 }
